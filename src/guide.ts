3ef85f3ac84b0a0a002cf76649fb08fb39e2d97d
KTibow <KTibow@users.noreply.github.com>
2025-04-07 10:42:41 -0700

    guide update

--- src/guide.ts
+++ src/guide.ts
@@ -3,37 +3,37 @@ const guide = `## Billing
 Right now, there are 3 plans:
 - Free with limits
 - Pay as you go
-(To get on this plan, go to https://console.groq.com, click "Chat with us" on bottom left, click Send us a message -> Account -> Dev Tier, and send your Organization ID)
+(To get on this plan, go to https://console.groq.com/settings/billing/plans)
 - Enterprise
 (To get on this plan, email \`sales@groq.com\`, although they may be slow to respond)
-
-In the future (I don't know when), Groq will have enough capacity for anyone to easily get on pay as you go. There will still be a free tier then. It might still be limit based or it might switch to credit based.
 
 ## Models and limits
 
 Groq currently has these models:
 
-- gemma-7b-it
+- qwen-2.5-32b
+- qwen-2.5-coder-32b
+- qwen-qwq-32b (best reasoning model)
+- deepseek-r1-distill-qwen-32b
+- deepseek-r1-distill-llama-70b
 - gemma2-9b-it
-- llama3-groq-70b-8192-tool-use-preview (good at "tool use", the practice of taking actions and retrieving information on behalf of the user; see https://console.groq.com/docs/tool-use)
-- llama3-groq-8b-8192-tool-use-preview (same as above)
 - distil-whisper-large-v3-en
-- llama-3.3-70b-versatile (usually the best model)
-- llama-3.1-70b-versatile
 - llama-3.1-8b-instant
 - llama-3.2-1b-preview
 - llama-3.2-3b-preview
 - llama-3.2-11b-vision-preview
 - llama-3.2-90b-vision-preview
+- llama-3.3-70b-versatile
 - llama-guard-3-8b (content moderation; see https://console.groq.com/docs/content-moderation)
 - llama3-70b-8192
 - llama3-8b-8192
-- mixtral-8x7b-32768
+- meta-llama/llama-4-scout-17b-16e-instruct (usually the best model)
+- mistral-saba-24b (good with middle east languages)
 - whisper-large-v3
 - whisper-large-v3-turbo
-- llava-v1.5-7b-4096-preview (this can see images, although it's not recent)
-
-Groq can't add new models with licenses that don't allow running them commercially (eg Mistral/Cohere models).
+- playai-tts
+- playai-tts-arabic
+- allam-2-7b (good with Arabic)
 
 The free tier has limits.
 
@@ -41,18 +41,16 @@ See: https://console.groq.com/docs/rate-limits
 
 ## Speed
 
-Smaller models are faster, but don't have as much world knowledge or intelligence. For example, Llama 3.2 1B runs at 3000 tokens/s, in comparison to Llama 3.1 70B at 250 tokens/s.
+Smaller models are faster, but don't have as much world knowledge or intelligence. For example, Llama 3.2 1B runs at 3000 tokens/s, in comparison to Llama 3.3 70B at 250 tokens/s.
 
-Speculative decoding can make Llama 70B run much faster. For predictable outputs, speculative decoding can reach 2500 tokens/s. In other scenarios, it's still faster, at 1660-2000 tokens/s. Speculative decoding is currently available in beta with some models for select customers (including pay-as-you-go), and can be accessed with the IDs "llama-3.3-70b-specdec" or "llama-3.1-70b-specdec". It doesn't lose accuracy, but it's slightly pricier at $0.99/million output tokens (compared to $0.79/million for standard sampling).
+Speculative decoding can make Llama run much faster. For predictable outputs, speculative decoding can reach 2500 tokens/s. In other scenarios, it's still faster, at 1660-2000 tokens/s. Speculative decoding is currently available in beta with some models for select customers (including pay-as-you-go), and can be accessed with the ID "llama-3.3-70b-specdec". It doesn't lose accuracy, but it's slightly pricier at $0.99/million output tokens (compared to $0.79/million for standard sampling).
 
 ## Planned and nonplanned feautres
 
-- Easy access to paid tiers: Once there's enough capacity (currently all going to running 70b for free)
-- Llama 405B: Previously ran, might run again once there's enough capacity
+- Llama 4 Maverick: Coming soon, but it's a large model
 - Embeddings: Currently being engineered
 - Faster performance: The current hardware is going as fast as it can. Groq is working on a new, thinner chip that will be faster.
 - Logprobs: Possible but not engineered yet
-- Text to speech: A common feature request but not engineered yet
 - Tokenization: Out of scope
 
 ## Accounts
